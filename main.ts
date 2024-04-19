9f807d10451448b4eef95b732a2b326931316f4c
Hafiz Shoaib Siddiq <159219257+student-shoaibsoomro@users.noreply.github.com>
2024-04-19 08:26:37 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+const answers: {
+    Sentence: "string"
+} = await inquirer.prompt(
+    [
+        {
+            name: "Sentence",
+            message: "Please enter your sentence here to count the word: ",
+            type: "input",
+        }
+    ]
+)
+
+const words = answers.Sentence.trim().split(" ");
+
+if(words.length === 1){
+    console.log(`Your Sentence Word Count is "${words.length}" Thank You for using this app.`);
+}
+else if(words.length >= 2){
+    console.log(`Your Sentence Words Counts are "${words.length}" ThankYou for using this app.`);
+}
+else{
+    console.log("You did not type any word Please enter your sentence first Thank You.")
+};
